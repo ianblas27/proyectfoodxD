4761eeb1673b3e83482b57c57a9f89ddc552972f
ianblas27 <34657613+ianblas27@users.noreply.github.com>
2018-02-04 01:57:56 -0500

    Componentes

--- w2cook/index.js
+++ w2cook/index.js
@@ -2,3 +2,18 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 
 AppRegistry.registerComponent('w2cook', () => App);
+import React, { Component } from 'react';
+import { Container, Button, Text } from 'native-base';
+export default class GeneralExample extends Component {
+  render() {
+    return (
+      <Container>
+        <Button>
+          <Text>
+            Button
+          </Text>
+        </Button>
+      </Container>
+    );
+  }
+}
